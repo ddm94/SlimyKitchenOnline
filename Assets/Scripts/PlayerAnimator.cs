d7da5691f637621a568e864c3cafb47305bc0f01
Daniele Di Modica <d.dimo94@hotmail.com>
2024-11-22 17:44:41 +0000

    Sync Player Animations
    Implemented client-authoritative synchronization for player animations.

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerAnimator : MonoBehaviour
+public class PlayerAnimator : NetworkBehaviour
 {
     private const string IS_WALKING = "IsWalking";
 
@@ -23,6 +22,9 @@ public class PlayerAnimator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!IsOwner)
+            return;
+
         animator.SetBool(IS_WALKING, player.IsWalking());
     }
 }
